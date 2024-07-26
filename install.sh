6944b35e1cf85a6424a4c6bb88af5760b08dfa66
TTR <git@cloudlive.us>
2024-07-26 15:38:42 -0400

    another commit

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@
 #Easy method to DOWNLOAD this: see README.md
 cd ~/
 git clone https://github.com/TheTechRun/Simple-i3
-wget https://raw.githubusercontent.com/TheTechRun/Simple-i3/master/app-install.sh
 mv ~/Simple-i3/.* ~/
-chmod +x app-install.sh
 chmod +x ~/.config/i3/* *.sh
 chmod +x ~/.config/polybar/* *.sh
-chmod +x ~/.scripts/rofi-themer.sh
+chmod +x ~/.scripts/* *.sh
